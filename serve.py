efcb5392cad2d7ea00a6b3cf33be4101a59e7607
Guillaume SANCHEZ <guillaume.sanchez@lighton.ai>
2024-11-12 05:23:58 +0100

    game UI works!

--- serve.py
+++ serve.py
@@ -1,11 +1,11 @@
 import pyximport
 
 pyximport.install(setup_args={"script_args": ["--cython-cplus"]})
-from engine import Game
+from engine import Game, RandomBuyStrategy
 
 # from main import Model
 import torch
-from fastapi import FastAPI
+from fastapi import FastAPI, Body
 from fastapi.responses import HTMLResponse, PlainTextResponse
 
 # m = Model()
@@ -15,6 +15,7 @@ from fastapi.responses import HTMLResponse, PlainTextResponse
 app = FastAPI()
 
 game = Game()
+strategy = RandomBuyStrategy()
 
 
 @app.get("/", response_class=HTMLResponse)
@@ -24,4 +25,12 @@ def read_root():
 
 @app.get("/board", response_class=PlainTextResponse)
 def board():
-    return game.display_with_movesdiscard()
+    return game.display_with_moves()
+
+
+@app.post("/do")
+def do(action: str = Body(..., embed=True)):
+    game.play_str(action)
+    move, _ = strategy(game)
+    game.play_str(move)
+    return True
