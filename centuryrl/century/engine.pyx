ce572da6bd4268b92a7ccf0a6260a911f93c98d8
Guillaume SANCHEZ <guillaume.sanchez@lighton.ai>
2024-12-30 03:28:45 +0100

    asserts are love

--- centuryrl/century/engine.pyx
+++ centuryrl/century/engine.pyx
@@ -764,8 +764,8 @@ cdef class Game:
 
     def display_with_moves(self, int force=-1) -> str:
         out = self.display(force=force) + '\n_Moves\n'
-        if force == -1 or force == self.current_player():
-            out += '\n'.join(['@'+ mov for mov in self.moves])
+        assert force == -1 or force == self.current_player()
+        out += ''.join(['@' for mov in self.moves])
         return out
 
     cpdef void buy_action(self, Player p, int idx, str give, str take):
