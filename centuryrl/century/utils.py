af8f7b448bb1dda2a28f4324ed87883ee19700ee
Guillaume SANCHEZ <guillaume.sanchez@lighton.ai>
2024-12-31 11:52:55 +0100

    make BatchProcessor behavior a bit more intuitive

--- centuryrl/century/utils.py
+++ centuryrl/century/utils.py
@@ -13,31 +13,28 @@ class BatchProcessor:
         self.batch_size = batch_size
         self.process_fn = process_fn
         self.queue = deque()
-        self.lock = asyncio.Lock()
         self.timeout = timeout
-        self.last_batch_time = asyncio.get_event_loop().time()
+        self.last_batch_time = None
 
     async def process_batch(self):
-        async with self.lock:
-            if len(self.queue) == 0:
-                return
+        if len(self.queue) == 0:
+            return
 
-            # Extract the current batch
-            batch = [
-                self.queue.popleft()
-                for _ in range(min(self.batch_size, len(self.queue)))
-            ]
-            inputs = [task["input"] for task in batch]
+        # Extract the current batch
+        batch = [
+            self.queue.popleft() for _ in range(min(self.batch_size, len(self.queue)))
+        ]
+        inputs = [task["input"] for task in batch]
 
-            # Process the batch
-            results = self.process_fn(inputs)
+        # Process the batch
+        results = self.process_fn(inputs)
 
-            # Update last batch processed time
-            self.last_batch_time = asyncio.get_event_loop().time()
+        # Update last batch processed time
+        self.last_batch_time = asyncio.get_event_loop().time()
 
-            # Return the results to the respective tasks
-            for task, result in zip(batch, results.unbatched()):
-                task["future"].set_result(result)
+        # Return the results to the respective tasks
+        for task, result in zip(batch, results.unbatched()):
+            task["future"].set_result(result)
 
     async def wait_data(self):
         if len(self.queue) >= self.batch_size or (
@@ -53,18 +50,20 @@ class BatchProcessor:
         # Add the task to the queue
         self.queue.append(task)
 
+        await self.wait_data()
         while not future.done():
-            # Check if the batch is ready to process
-            await self.wait_data()
-
             # Sleep briefly to prevent busy-waiting
             await asyncio.sleep(0.01)
+
+            # Check if the batch is ready to process
+            await self.wait_data()
 
         # Wait for the result
         return await future
 
     def run_tasks(self, tasks):
         async def do():
+            self.last_batch_time = asyncio.get_event_loop().time()
             return await asyncio.gather(*[asyncio.create_task(t) for t in tasks])
 
         ret = asyncio.run(do())
