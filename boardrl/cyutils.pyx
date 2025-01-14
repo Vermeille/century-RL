4d96f8517ec16391cd5e5451da3488e15589370c
Guillaume "Vermeille" Sanchez <guillaume.v.sanchez@gmail.com>
2025-01-14 16:20:14 +0100

    better for numerical precision

--- boardrl/cyutils.pyx
+++ boardrl/cyutils.pyx
@@ -22,7 +22,7 @@ cpdef int fast_sample(x):
     for i in range(n):
         total += x_[i]
 
-    r = rand() / RAND_MAX * total
+    r = rand() * total / RAND_MAX
     for i in range(n):
         acc += x_[i]
         if acc >= r:
