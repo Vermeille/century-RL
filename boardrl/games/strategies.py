b32df081179b83afae0b29dfffd8a505f5f3a12a
Guillaume SANCHEZ <guillaume.sanchez@lighton.ai>
2025-08-06 03:11:22 +0200

    fix temperature

--- boardrl/games/strategies.py
+++ boardrl/games/strategies.py
@@ -122,8 +122,7 @@ class PolicySamplingStrategy:
 
         policy = (
             (await self.nn(g.display_with_moves())).policy[0].cpu()
-        )  # / self.temperature
-        print(policy)
+        ) / self.temperature
         return policy, {"moves": dict(zip(g.moves, policy.tolist()))}
 
 
