9bf3167f0163ff9f149e2636fb2b836f6b20a69f
Guillaume "Vermeille" Sanchez <Guillaume.V.Sanchez@gmail.com>
2025-08-10 21:10:10 +0100

    refactor: move model pool to utils

--- boardrl/serve/serve.py
+++ boardrl/serve/serve.py
@@ -10,7 +10,7 @@ import pyximport
 pyximport.install(setup_args={"script_args": ["--cython-cplus"]})
 from boardrl.games import games_library
 from boardrl.cyutils import fast_sample
-from boardrl.games.strategies import ModelPool
+from boardrl.utils import ModelPool
 
 
 class Strategies:
