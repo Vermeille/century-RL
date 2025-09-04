156927796af5369d4f21dfd88dc083a591799de8
Guillaume "Vermeille" Sanchez <Guillaume.V.Sanchez@gmail.com>
2025-09-04 13:00:13 +0200

    Update tests for new loss configuration

--- tests/test_policy_gradient_loss.py
+++ tests/test_policy_gradient_loss.py
@@ -1,17 +1,20 @@
 import torch
 import torch.nn.functional as F
 from types import SimpleNamespace
-from boardrl.rl.model.loss import PolicyGradientLoss
-from boardrl.utils import entropy
+from boardrl.rl.model.loss import (
+    PolicyGradientLoss,
+    EntropyBonus,
+    KLPenalty,
+)
 
 
 def test_entropy_regularizer_zero_grad_at_uniform():
     logits = torch.zeros(4, requires_grad=True)
     sample = SimpleNamespace(action_idx=[torch.tensor(0)], returns=torch.tensor([0.0]))
-    loss_fn = PolicyGradientLoss(
-        weight="returns", entropy_bonus=1.0, aux_logits_coef=0.0
-    )
-    loss = loss_fn([logits], None, sample)
+    pred_value = SimpleNamespace(mean=torch.tensor([0.0]))
+    policy = PolicyGradientLoss(weight="returns")
+    entropy_bonus = EntropyBonus(strength=1.0)
+    loss = policy([logits], pred_value, sample) + entropy_bonus([logits], pred_value, sample)
     loss.backward()
     assert torch.allclose(logits.grad, torch.zeros_like(logits), atol=1e-6)
 
@@ -20,13 +23,13 @@ def test_entropy_regularizer_drives_uniform_policy():
     torch.manual_seed(0)
     logits = torch.randn(5, requires_grad=True)
     sample = SimpleNamespace(action_idx=[torch.tensor(0)], returns=torch.tensor([0.0]))
-    loss_fn = PolicyGradientLoss(
-        weight="returns", entropy_bonus=1.0, aux_logits_coef=0.0
-    )
+    pred_value = SimpleNamespace(mean=torch.tensor([0.0]))
+    policy = PolicyGradientLoss(weight="returns")
+    entropy_bonus = EntropyBonus(strength=1.0)
     opt = torch.optim.SGD([logits], lr=0.2)
     for _ in range(400):
         opt.zero_grad()
-        loss = loss_fn([logits], None, sample)
+        loss = policy([logits], pred_value, sample) + entropy_bonus([logits], pred_value, sample)
         loss.backward()
         opt.step()
     probs = logits.softmax(dim=0)
@@ -43,12 +46,10 @@ def test_kl_regularizer_matches_manual():
         reference_policy=[reference_logits],
     )
 
-    loss_fn = PolicyGradientLoss(
-        weight="returns",
-        kl_strength=0.5,
-        aux_logits_coef=0.0,
-    )
-    loss = loss_fn([logits], None, sample)
+    pred_value = SimpleNamespace(mean=torch.tensor([0.0]))
+    policy = PolicyGradientLoss(weight="returns")
+    kl_penalty = KLPenalty(strength=0.5)
+    loss = policy([logits], pred_value, sample) + kl_penalty([logits], pred_value, sample)
     ce = F.cross_entropy(logits, sample.action_idx[0], label_smoothing=0.002)
     kl = F.kl_div(
         F.log_softmax(logits, dim=0),
