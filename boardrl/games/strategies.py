6fcae36d2ddd0de773651218e260db25534a611a
Guillaume "Vermeille" Sanchez <guillaume.v.sanchez@gmail.com>
2026-07-29 14:11:27 +0200

    fix(strategies): preserve default model resolution

--- boardrl/games/strategies.py
+++ boardrl/games/strategies.py
@@ -16,7 +16,7 @@ def get_model(arg_str, default, provided_arg):
     """
 
     assert isinstance(provided_arg, ModelPool)
-    return provided_arg(unquote(arg_str))
+    return provided_arg(unquote(arg_str) if arg_str is not None else None)
 
 
 strategy_from_string = RegisterByName(arg_readers={"model": get_model})
