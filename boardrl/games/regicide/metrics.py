7d8576ce0ef3ebaae05a292d3bdfaec46b4ff8ae
Guillaume "Vermeille" Sanchez <Guillaume.V.Sanchez@gmail.com>
2026-09-07 00:12:49 +0200

    Revert accidental Regicide metric update on master

--- boardrl/games/regicide/metrics.py
+++ boardrl/games/regicide/metrics.py
@@ -1,18 +1,4 @@
-from itertools import accumulate
-
-from boardrl.games.regicide.game import ENEMY_HP
 from boardrl.metrics import GameMetrics, Range
-
-
-ENEMY_DEFEAT_THRESHOLDS = tuple(
-    accumulate(
-        [ENEMY_HP[rank] for rank in ("J",) * 4 + ("Q",) * 4 + ("K",) * 4]
-    )
-)
-
-
-def enemies_defeated(points: float) -> int:
-    return sum(points >= threshold for threshold in ENEMY_DEFEAT_THRESHOLDS)
 
 
 class Metrics(GameMetrics):
@@ -24,6 +10,5 @@ class Metrics(GameMetrics):
         rounds = [game[0][-1].round for game in self.data]
         return {
             "points": Range(points),
-            "enemies_defeated": Range([enemies_defeated(score) for score in points]),
             "rounds": Range(rounds),
         }
