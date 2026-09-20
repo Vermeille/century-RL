fffb242d5470b4fea7062892b320dbd01d2fd6b7
Guillaume "Vermeille" Sanchez <Guillaume.V.Sanchez@gmail.com>
2026-09-20 15:31:48 +0200

    Test Century hand and discard augmentations separately

--- tests/test_century_augmentation.py
+++ tests/test_century_augmentation.py
@@ -1,14 +1,20 @@
 import torch
 
-from boardrl.games.century.augmentations import shuffle_owned_cards
+from boardrl.games import games_library
+from boardrl.games.augmentations import shuffle_actions
+from boardrl.games.century.augmentations import shuffle_discard, shuffle_hand
 from boardrl.training import TrainingSample
 
 
-def test_shuffle_owned_cards_remaps_hand_moves_and_shuffles_discard(monkeypatch):
+def _reverse_shuffle(monkeypatch):
     monkeypatch.setattr(
         "boardrl.games.century.augmentations.random.shuffle",
         lambda values: values.reverse(),
     )
+
+
+def test_shuffle_hand_remaps_hand_moves_without_touching_discard(monkeypatch):
+    _reverse_shuffle(monkeypatch)
     sample = TrainingSample(
         state=(
             "0    4\n"
@@ -36,7 +42,7 @@ def test_shuffle_owned_cards_remaps_hand_moves_and_shuffles_discard(monkeypatch)
         reference_policy=torch.tensor([0.3, 0.4, 0.3]),
     )
 
-    augmented = shuffle_owned_cards([sample])[0]
+    augmented = shuffle_hand([sample])[0]
 
     assert augmented is not sample
     assert augmented.state == (
@@ -52,8 +58,8 @@ def test_shuffle_owned_cards_remaps_hand_moves_and_shuffles_discard(monkeypatch)
         "H0 G>RR\n"
         "H1 XX\n"
         "H2 >YY\n"
-        "D0 >B\n"
-        "D1 YY>R\n"
+        "D0 YY>R\n"
+        "D1 >B\n"
         "_Moves\n"
         "@R\n"
         "@H2 >YY\n"
@@ -69,18 +75,48 @@ def test_shuffle_owned_cards_remaps_hand_moves_and_shuffles_discard(monkeypatch)
     assert "D0 YY>R\nD1 >B" in sample.state
 
 
-def test_shuffle_owned_cards_preserves_line_endings(monkeypatch):
-    monkeypatch.setattr(
-        "boardrl.games.century.augmentations.random.shuffle",
-        lambda values: values.reverse(),
+def test_shuffle_discard_does_not_touch_hand_or_moves(monkeypatch):
+    _reverse_shuffle(monkeypatch)
+    sample = TrainingSample(
+        state=(
+            "_Me 0\n"
+            "H0 >YY\n"
+            "H1 G>RR\n"
+            "D0 YY>R\n"
+            "D1 >B\n"
+            "_Moves\n"
+            "@H0 >YY\n"
+            "@H1 G>RR"
+        ),
+        moves=["H0 >YY", "H1 G>RR"],
+        action_idx=1,
+        action_distribution=torch.tensor([0.4, 0.6]),
     )
+
+    augmented = shuffle_discard([sample])[0]
+
+    assert augmented.state == (
+        "_Me 0\n"
+        "H0 >YY\n"
+        "H1 G>RR\n"
+        "D0 >B\n"
+        "D1 YY>R\n"
+        "_Moves\n"
+        "@H0 >YY\n"
+        "@H1 G>RR"
+    )
+    assert augmented.moves == sample.moves
+    assert augmented.action_idx == sample.action_idx
+    assert torch.equal(augmented.action_distribution, sample.action_distribution)
+
+
+def test_shuffle_hand_preserves_line_endings(monkeypatch):
+    _reverse_shuffle(monkeypatch)
     sample = TrainingSample(
         state=(
             "_Me 0\r\n"
             "H0 >YY\r\n"
             "H1 G>RR\r\n"
-            "D0 >R\r\n"
-            "D1 >G\r\n"
             "_Moves\r\n"
             "@H0 >YY\r\n"
             "@H1 G>RR\r\n"
@@ -89,36 +125,64 @@ def test_shuffle_owned_cards_preserves_line_endings(monkeypatch):
         action_idx=0,
     )
 
-    augmented = shuffle_owned_cards([sample])[0]
+    augmented = shuffle_hand([sample])[0]
 
     assert augmented.state == (
         "_Me 0\r\n"
         "H0 G>RR\r\n"
         "H1 >YY\r\n"
-        "D0 >G\r\n"
-        "D1 >R\r\n"
         "_Moves\r\n"
         "@H1 >YY\r\n"
         "@H0 G>RR\r\n"
     )
     assert augmented.moves == ("H1 >YY", "H0 G>RR")
-    assert augmented.action_idx == 0
 
 
-def test_shuffle_owned_cards_without_me_section_is_noop(monkeypatch):
-    monkeypatch.setattr(
-        "boardrl.games.century.augmentations.random.shuffle",
-        lambda values: values.reverse(),
-    )
+def test_shuffle_discard_preserves_line_endings(monkeypatch):
+    _reverse_shuffle(monkeypatch)
     sample = TrainingSample(
-        state="H0 >YY\nH1 G>RR\n@H0 >YY\n@H1 G>RR",
+        state=(
+            "_Me 0\r\n"
+            "D0 >R\r\n"
+            "D1 >G\r\n"
+            "_Moves\r\n"
+            "@R\r\n"
+        ),
+        action_idx=0,
+    )
+
+    augmented = shuffle_discard([sample])[0]
+
+    assert augmented.state == (
+        "_Me 0\r\n"
+        "D0 >G\r\n"
+        "D1 >R\r\n"
+        "_Moves\r\n"
+        "@R\r\n"
+    )
+
+
+def test_century_card_shuffles_without_me_section_are_noops(monkeypatch):
+    _reverse_shuffle(monkeypatch)
+    sample = TrainingSample(
+        state="H0 >YY\nH1 G>RR\nD0 >R\nD1 >G\n@H0 >YY\n@H1 G>RR",
         moves=["H0 >YY", "H1 G>RR"],
         action_idx=1,
     )
 
-    augmented = shuffle_owned_cards([sample])[0]
+    hand_augmented = shuffle_hand([sample])[0]
+    discard_augmented = shuffle_discard([sample])[0]
 
-    assert augmented is not sample
-    assert augmented.state == sample.state
-    assert augmented.moves == sample.moves
-    assert augmented.action_idx == sample.action_idx
+    for augmented in (hand_augmented, discard_augmented):
+        assert augmented is not sample
+        assert augmented.state == sample.state
+        assert augmented.moves == sample.moves
+        assert augmented.action_idx == sample.action_idx
+
+
+def test_century_registers_hand_and_discard_augmentations_separately():
+    assert games_library("century").augmentations == (
+        shuffle_actions,
+        shuffle_hand,
+        shuffle_discard,
+    )
