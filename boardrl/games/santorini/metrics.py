fb9847e426aeb46c7a51d8b6a4b1d2c4ac9127c4
Guillaume "Vermeille" Sanchez <Guillaume.V.Sanchez@gmail.com>
2026-09-21 00:09:16 +0200

    Add Santorini metrics

---
+++ boardrl/games/santorini/metrics.py
@@ -0,0 +1,28 @@
+from boardrl.games.semantics import OutcomeScores
+from boardrl.metrics import GameMetrics, GroupedTraceMetrics, TraceMetrics
+
+
+class Metrics(GameMetrics):
+    def __init__(self, data):
+        self.data = data
+
+    def print_short_history(self):
+        for game in self.data:
+            terminal = game.by_seat[0][-1]
+            print(terminal.state, terminal.my_points)
+            print()
+
+    def metrics(self):
+        terminal_games = sum(game.by_seat[0][-1].terminal for game in self.data)
+        return {
+            "terminal_rate": terminal_games / len(self.data),
+            "avg_game_actions": sum(
+                max(len(trace) - 1, 0)
+                for game in self.data
+                for trace in game.by_seat
+            )
+            / len(self.data),
+            "strategy": GroupedTraceMetrics(
+                self.data.by_strategy, TraceMetrics, scores=OutcomeScores()
+            ).metrics(),
+        }
